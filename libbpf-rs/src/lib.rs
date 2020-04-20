09e223ee0f0aeb5b2750aba07a895f632089cb11
Daniel Xu <dxu@dxuuu.xyz>
2020-04-19 22:15:58 -0700

    Add crate doc

--- libbpf-rs/src/lib.rs
+++ libbpf-rs/src/lib.rs
@@ -1,3 +1,14 @@
+//! # libbpf-rs
+//!
+//! `libbpf-rs` is a safe, idiomatic, and opinionated wrapper around
+//! [libbpf](https://github.com/libbpf/libbpf/).
+//!
+//! libbpf-rs, together with libbpf-cargo (libbpf cargo plugin) allow you
+//! to write Compile-Once-Run-Everywhere (CO-RE) eBPF programs.
+//!
+//! More information about CO-RE is [available
+//! here](https://facebookmicrosites.github.io/bpf/blog/2020/02/19/bpf-portability-and-co-re.html).
+
 mod error;
 mod link;
 mod object;
