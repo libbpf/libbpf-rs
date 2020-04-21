6480aa1bbef6836aa5dc58302dccf82207e861e3
Daniel Xu <dxu@dxuuu.xyz>
2020-04-21 10:01:56 -0700

    Link doc

--- libbpf-rs/src/link.rs
+++ libbpf-rs/src/link.rs
@@ -1,7 +1,7 @@
 use crate::object::{Object, Program};
 use crate::*;
 
-/// Represents an attached [`Program`].
+/// Represents a loaded and attached [`Program`].
 ///
 /// This struct is used to model ownership. The underlying program will be detached
 /// and unloaded from the system when this object is dropped.
