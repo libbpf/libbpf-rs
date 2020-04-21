12f8ba88a4771cb6a76a99f1472cb4501f5d6989
Daniel Xu <dxu@dxuuu.xyz>
2020-04-21 10:08:39 -0700

    Doc wording

--- libbpf-rs/src/error.rs
+++ libbpf-rs/src/error.rs
@@ -3,7 +3,7 @@ use std::result;
 
 use thiserror::Error;
 
-/// The canonical error type for this crate.
+/// Canonical error type for this crate.
 #[derive(Error, Debug)]
 pub enum Error {
     #[error("I/O error")]
