cacb2e822aeaf042b5ad9f1dfc6970350d3744cb
Daniel Xu <dxu@dxuuu.xyz>
2020-04-21 10:08:07 -0700

    libbpf-rs: Export MapType

--- libbpf-rs/src/lib.rs
+++ libbpf-rs/src/lib.rs
@@ -18,6 +18,6 @@ mod perf_buffer;
 pub use crate::error::{Error, Result};
 pub use crate::link::Link;
 pub use crate::object::{
-    Map, MapOptions, Object, ObjectOptions, Program, ProgramAttachType, ProgramType,
+    Map, MapOptions, MapType, Object, ObjectOptions, Program, ProgramAttachType, ProgramType,
 };
 pub use crate::perf_buffer::{PerfBuffer, PerfBufferOpts};
