9204a6df4df777b33b2ecc56ba72629478f4bbbd
Daniel Xu <dxu@dxuuu.xyz>
2020-05-04 15:49:59 -0700

    libbpf-rs: Add Map tests

--- libbpf-rs/tests/test.rs
+++ libbpf-rs/tests/test.rs
@@ -1,7 +1,7 @@
 use std::fs;
 use std::path::PathBuf;
 
-use libbpf_rs::{Object, ObjectBuilder};
+use libbpf_rs::{MapFlags, Object, ObjectBuilder};
 
 fn get_test_object_path() -> PathBuf {
     let mut path = PathBuf::new();
@@ -15,6 +15,16 @@ fn get_test_object() -> Object {
     let obj_path = get_test_object_path();
     let mut builder = ObjectBuilder::default();
     builder.from_path(obj_path).expect("failed to build object")
+}
+
+fn bump_rlimit_mlock() {
+    let rlimit = libc::rlimit {
+        rlim_cur: 128 << 20,
+        rlim_max: 128 << 20,
+    };
+
+    let ret = unsafe { libc::setrlimit(libc::RLIMIT_MEMLOCK, &rlimit) };
+    assert!(ret == 0);
 }
 
 #[test]
@@ -54,6 +64,112 @@ fn test_object_maps() {
         .expect("error finding map")
         .expect("failed to find map");
     assert!(obj.map("asdf").expect("error finding map").is_none());
+}
+
+#[test]
+fn test_object_map_load() {
+    bump_rlimit_mlock();
+
+    let mut obj = get_test_object();
+    let _ = obj
+        .map("start")
+        .expect("error finding map")
+        .expect("failed to find map")
+        .load()
+        .expect("failed to load start map");
+    let _ = obj
+        .map("events")
+        .expect("error finding map")
+        .expect("failed to find map")
+        .load()
+        .expect("failed to load events map");
+}
+
+#[test]
+fn test_object_map_key_value_size() {
+    bump_rlimit_mlock();
+
+    let mut obj = get_test_object();
+    let mut start = obj
+        .map("start")
+        .expect("error finding map")
+        .expect("failed to find map")
+        .load()
+        .expect("failed to load start map");
+
+    assert!(start.lookup(&[1, 2, 3, 4, 5], MapFlags::empty()).is_err());
+    assert!(start.delete(&[1]).is_err());
+    assert!(start.lookup_and_delete(&[1, 2, 3, 4, 5]).is_err());
+    assert!(start
+        .update(&[1, 2, 3, 4, 5], &[1], MapFlags::empty())
+        .is_err());
+}
+
+#[test]
+fn test_object_map_empty_lookup() {
+    bump_rlimit_mlock();
+
+    let mut obj = get_test_object();
+    let start = obj
+        .map("start")
+        .expect("error finding map")
+        .expect("failed to find map")
+        .load()
+        .expect("failed to load start map");
+    assert!(start
+        .lookup(&[1, 2, 3, 4], MapFlags::empty())
+        .expect("err in map lookup")
+        .is_none());
+}
+
+#[test]
+fn test_object_map_mutation() {
+    bump_rlimit_mlock();
+
+    let mut obj = get_test_object();
+    let mut start = obj
+        .map("start")
+        .expect("error finding map")
+        .expect("failed to find map")
+        .load()
+        .expect("failed to load start map");
+
+    start
+        .update(&[1, 2, 3, 4], &[1, 2, 3, 4, 5, 6, 7, 8], MapFlags::empty())
+        .expect("failed to write");
+    let val = start
+        .lookup(&[1, 2, 3, 4], MapFlags::empty())
+        .expect("failed to read map")
+        .expect("failed to find key");
+    assert_eq!(val.len(), 8);
+    assert_eq!(val, &[1, 2, 3, 4, 5, 6, 7, 8]);
+
+    start.delete(&[1, 2, 3, 4]).expect("failed to delete key");
+
+    assert!(start
+        .lookup(&[1, 2, 3, 4], MapFlags::empty())
+        .expect("failed to read map")
+        .is_none());
+}
+
+#[test]
+fn test_object_map_lookup_flags() {
+    bump_rlimit_mlock();
+
+    let mut obj = get_test_object();
+    let mut start = obj
+        .map("start")
+        .expect("error finding map")
+        .expect("failed to find map")
+        .load()
+        .expect("failed to load start map");
+
+    start
+        .update(&[1, 2, 3, 4], &[1, 2, 3, 4, 5, 6, 7, 8], MapFlags::NO_EXIST)
+        .expect("failed to write");
+    assert!(start
+        .update(&[1, 2, 3, 4], &[1, 2, 3, 4, 5, 6, 7, 8], MapFlags::NO_EXIST)
+        .is_err());
 }
 
 #[test]
