f1a60ad1eed965e55dc9a7e34a0103f1c2623a68
Daniel Xu <dxu@dxuuu.xyz>
2020-06-09 12:11:14 -0700

    example: Implement runqslower
    This commit implements runqslower using libbpf-rs.
    
    Example output:
    ```
    $ ./target/debug/example --help
    example 0.1.0
    Trace high run queue latency
    
    USAGE:
        example [FLAGS] [OPTIONS] [latency]
    
    FLAGS:
        -h, --help       Prints help information
        -V, --version    Prints version information
        -v, --verbose    Verbose debug output
    
    OPTIONS:
        -o, --obj-path <obj-path>    Path to runqslower bpf object file [default: /bin/runqslower.bpf.o]
    
    ARGS:
        <latency>    Trace latency higher than this value [default: 10000]
    
    $ sudo ./target/debug/example -o ~/dev/libbpf-rs/target/bpf/runqslower.bpf.o 1000
    Tracing run queue latency higher than 1000 us
    TIME     COMM             TID     LAT(us)
    13:32:43 Gecko_IOThread   961074  1009
    13:32:43 Compositor       961132  2357
    13:32:43 Gecko_IOThread   961074  1546
    13:32:43 Compositor       961132  1464
    13:32:43 Gecko_IOThread   961074  1763
    13:32:43 example          1416001 3966
    13:32:43 IPDL Background  961135  4084
    13:32:43 rcu_sched        11      1387
    13:32:43 rcu_sched        11      2051
    13:32:43 scribbled        89060   2306
    13:32:43 Compositor       961132  4550
    13:32:43 scribbled        89060   1183
    13:32:43 scribbled        89057   1082
    ^C
    ```

--- example/src/main.rs
+++ example/src/main.rs
@@ -1,3 +1,127 @@
+// SPDX-License-Identifier: (LGPL-2.1 OR BSD-2-Clause)
+
+use core::time::Duration;
+use std::path::PathBuf;
+use std::process::exit;
+
+use anyhow::{bail, Result};
+use chrono::Local;
+use libbpf_rs::{MapFlags, ObjectBuilder, PerfBufferBuilder};
+use plain::Plain;
+use structopt::StructOpt;
+
+/// Trace high run queue latency
+#[derive(Debug, StructOpt)]
+struct Command {
+    /// Trace latency higher than this value
+    #[structopt(default_value = "10000")]
+    latency: u64,
+    /// Verbose debug output
+    #[structopt(short, long)]
+    verbose: bool,
+    /// Path to runqslower bpf object file
+    #[structopt(short, long, default_value = "/bin/runqslower.bpf.o")]
+    obj_path: PathBuf,
+}
+
+#[repr(C)]
+#[derive(Default)]
+struct Event {
+    pub task: [u8; 16],
+    pub delta_us: u64,
+    pub pid: i32,
+}
+
+unsafe impl Plain for Event {}
+
+fn bump_memlock_rlimit() -> Result<()> {
+    let rlimit = libc::rlimit {
+        rlim_cur: 128 << 20,
+        rlim_max: 128 << 20,
+    };
+
+    if unsafe { libc::setrlimit(libc::RLIMIT_MEMLOCK, &rlimit) } != 0 {
+        bail!("Failed to increase rlimit");
+    }
+
+    Ok(())
+}
+
+fn handle_event(_cpu: i32, data: &[u8]) {
+    let mut event = Event::default();
+    plain::copy_from_bytes(&mut event, data).expect("Data buffer was too short");
+
+    let now = Local::now();
+    let task = std::str::from_utf8(&event.task).unwrap();
+
+    println!(
+        "{:8} {:16} {:<7} {:<14}",
+        now.format("%H:%M:%S"),
+        task.trim_end_matches(char::from(0)),
+        event.pid,
+        event.delta_us
+    );
+}
+
+fn handle_lost_events(cpu: i32, count: u64) {
+    eprintln!("Lost {} events on CPU {}", count, cpu);
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opts = Command::from_args();
+
+    if !opts.obj_path.as_path().exists() {
+        eprintln!("{} does not exist", opts.obj_path.as_path().display());
+        exit(1);
+    }
+
+    let mut obj_builder = ObjectBuilder::default();
+    if opts.verbose {
+        obj_builder.set_debug(true);
+    }
+
+    bump_memlock_rlimit().unwrap();
+    let mut obj = obj_builder
+        .from_path(opts.obj_path)
+        .unwrap()
+        .load()
+        .unwrap();
+
+    // Write latency value into map
+    obj.map_unwrap("min_us")
+        .update(
+            &0u32.to_le_bytes(),
+            &opts.latency.to_le_bytes(),
+            MapFlags::empty(),
+        )
+        .unwrap();
+
+    // NB it's crucial that these are named underscore variables otherwise
+    // the link is immediately dropped and our progs aren't run.
+    let _wakup_link = obj.prog_unwrap("handle__sched_wakeup").attach().unwrap();
+    let _wakeup_new_link = obj
+        .prog_unwrap("handle__sched_wakeup_new")
+        .attach()
+        .unwrap();
+    let _switch_link = obj.prog_unwrap("handle__sched_switch").attach().unwrap();
+
+    println!("Tracing run queue latency higher than {} us", opts.latency);
+    println!("{:8} {:16} {:7} {:14}", "TIME", "COMM", "TID", "LAT(us)");
+
+    let events = obj.map_unwrap("events");
+    let mut perf_builder = PerfBufferBuilder::new(events);
+    perf_builder.set_sample_cb(handle_event);
+    perf_builder.set_lost_cb(handle_lost_events);
+    let perf = perf_builder.build().unwrap();
+
+    loop {
+        let ret = perf.poll(Duration::from_millis(100));
+        match ret {
+            Ok(()) => (),
+            Err(e) => {
+                eprintln!("Error polling perf buffer: {}", e);
+                exit(1);
+            }
+        };
+    }
 }
