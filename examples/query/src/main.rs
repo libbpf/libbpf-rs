d7404b64c684c1f75610cbf5895ab20f5d117df4
Daniel Xu <dxu@dxuuu.xyz>
2020-07-09 10:57:40 -0700

    examples: Add query example

--- examples/query/src/main.rs
+++ examples/query/src/main.rs
@@ -1,3 +1,55 @@
+use std::process::exit;
+
+use libbpf_rs::query;
+use nix::unistd::Uid;
+use structopt::StructOpt;
+
+/// Query the system about BPF-related information
+#[derive(Debug, StructOpt)]
+enum Command {
+    /// Display information about progs
+    Prog,
+    /// Display information about maps
+    Map,
+    /// Display information about BTF
+    Btf,
+}
+
+fn prog() {
+    for prog in query::ProgInfoIter::default() {
+        println!(
+            "name={:<16} type={:<15} run_count={:<2} runtime_ns={}",
+            prog.name,
+            prog.ty.to_string(),
+            prog.run_cnt,
+            prog.run_time_ns
+        );
+    }
+}
+
+fn map() {
+    for map in query::MapInfoIter::default() {
+        println!("name={:<16} type={}", map.name, map.ty.to_string(),);
+    }
+}
+
+fn btf() {
+    for btf in query::BtfInfoIter::default() {
+        println!("id={:4} size={}", btf.id, btf.btf_size);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    if !Uid::effective().is_root() {
+        eprintln!("Must run as root");
+        exit(1);
+    }
+
+    let opts = Command::from_args();
+
+    match opts {
+        Command::Prog => prog(),
+        Command::Map => map(),
+        Command::Btf => btf(),
+    };
 }
