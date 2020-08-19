7decfdc433220ea13eb7cb36865b899e6c5aef1a
Daniel Xu <dxu@dxuuu.xyz>
2020-08-19 13:21:56 -0700

    libbpf-cargo: gen: Generate basic skeleton
    This commit generates a basic skeleton that has:
    * the embedded object file
    * map/prog getters

--- libbpf-cargo/src/gen.rs
+++ libbpf-cargo/src/gen.rs
@@ -1,10 +1,12 @@
+use std::ffi::{CStr, CString};
 use std::fmt::Write as fmt_write;
 use std::fs::File;
 use std::io::Write;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 use std::process::{Command, Stdio};
+use std::ptr;
 
-use anyhow::Result;
+use anyhow::{bail, Result};
 
 use crate::metadata;
 use crate::metadata::UnprocessedObj;
@@ -80,8 +82,195 @@ fn get_prog_name(prog: *const libbpf_sys::bpf_program) -> Result<String> {
     Ok(unsafe { CStr::from_ptr(name_ptr) }.to_str()?.to_string())
 }
 
+fn gen_skel_map_defs(
+    skel: &mut String,
+    object: *mut libbpf_sys::bpf_object,
+    obj_name: &str,
+    open: bool,
+) -> Result<()> {
+    if MapIter::new(object).next().is_none() {
+        return Ok(());
+    }
+
+    let (struct_name, inner_ty, return_ty) = if open {
+        (
+            format!("Open{}Maps", obj_name),
+            "libbpf_rs::OpenObject",
+            "libbpf_rs::OpenMap",
+        )
+    } else {
+        (
+            format!("{}Maps", obj_name),
+            "libbpf_rs::Object",
+            "libbpf_rs::Map",
+        )
+    };
+
+    write!(
+        skel,
+        r#"
+        pub struct {struct_name}<'a> {{
+            inner: &'a mut {inner_ty},
+        }}
+
+        impl<'a> {struct_name}<'a> {{
+        "#,
+        inner_ty = inner_ty,
+        struct_name = struct_name
+    )?;
+
+    for map in MapIter::new(object) {
+        write!(
+            skel,
+            r#"
+            pub fn {map_name}(&mut self) -> &mut {return_ty} {{
+                self.inner.map_unwrap("{map_name}")
+            }}
+            "#,
+            map_name = get_map_name(map)?,
+            return_ty = return_ty,
+        )?;
+    }
+
+    writeln!(skel, "}}")?;
+
+    Ok(())
+}
+
+fn gen_skel_prog_defs(
+    skel: &mut String,
+    object: *mut libbpf_sys::bpf_object,
+    obj_name: &str,
+    open: bool,
+) -> Result<()> {
+    if ProgIter::new(object).next().is_none() {
+        return Ok(());
+    }
+
+    let (struct_name, inner_ty, return_ty) = if open {
+        (
+            format!("Open{}Progs", obj_name),
+            "libbpf_rs::OpenObject",
+            "libbpf_rs::OpenProgram",
+        )
+    } else {
+        (
+            format!("{}Progs", obj_name),
+            "libbpf_rs::Object",
+            "libbpf_rs::Program",
+        )
+    };
+
+    write!(
+        skel,
+        r#"
+        pub struct {struct_name}<'a> {{
+            inner: &'a mut {inner_ty},
+        }}
+
+        impl<'a> {struct_name}<'a> {{
+        "#,
+        inner_ty = inner_ty,
+        struct_name = struct_name
+    )?;
+
+    for prog in ProgIter::new(object) {
+        write!(
+            skel,
+            r#"
+            pub fn {prog_name}(&mut self) -> &mut {return_ty} {{
+                self.inner.prog_unwrap("{prog_name}")
+            }}
+            "#,
+            prog_name = get_prog_name(prog)?,
+            return_ty = return_ty,
+        )?;
+    }
+
+    writeln!(skel, "}}")?;
+
+    Ok(())
+}
+
+fn gen_skel_map_getter(
+    skel: &mut String,
+    object: *mut libbpf_sys::bpf_object,
+    obj_name: &str,
+    open: bool,
+) -> Result<()> {
+    if MapIter::new(object).next().is_none() {
+        return Ok(());
+    }
+
+    let return_ty = if open {
+        format!("Open{}Maps", obj_name)
+    } else {
+        format!("{}Maps", obj_name)
+    };
+
+    write!(
+        skel,
+        r#"
+        pub fn maps(&mut self) -> {return_ty} {{
+            {return_ty} {{
+                inner: &mut self.obj,
+            }}
+        }}
+        "#,
+        return_ty = return_ty
+    )?;
+
+    Ok(())
+}
+
+fn gen_skel_prog_getter(
+    skel: &mut String,
+    object: *mut libbpf_sys::bpf_object,
+    obj_name: &str,
+    open: bool,
+) -> Result<()> {
+    if ProgIter::new(object).next().is_none() {
+        return Ok(());
+    }
+
+    let return_ty = if open {
+        format!("Open{}Progs", obj_name)
+    } else {
+        format!("{}Progs", obj_name)
+    };
+
+    write!(
+        skel,
+        r#"
+        pub fn progs(&mut self) -> {return_ty} {{
+            {return_ty} {{
+                inner: &mut self.obj,
+            }}
+        }}
+        "#,
+        return_ty = return_ty
+    )?;
+
+    Ok(())
+}
+
+fn open_object_file(path: &Path) -> Result<*mut libbpf_sys::bpf_object> {
+    if !path.exists() {
+        bail!("Object file not found: {}", path.display());
+    }
+    let path_cstring = CString::new(path.to_string_lossy().into_owned())?;
+    let mut obj_opts = libbpf_sys::bpf_object_open_opts::default();
+    obj_opts.sz = std::mem::size_of::<libbpf_sys::bpf_object_open_opts>() as libbpf_sys::size_t;
+    let object = unsafe { libbpf_sys::bpf_object__open_file(path_cstring.as_ptr(), &obj_opts) };
+    if object.is_null() {
+        bail!("Could not open object file={}", path.display());
+    }
+
+    Ok(object)
+}
+
 /// Generate contents of a single skeleton
-fn gen_skel_contents(_debug: bool, _obj: &UnprocessedObj) -> Result<String> {
+fn gen_skel_contents(_debug: bool, obj: &UnprocessedObj) -> Result<String> {
     let mut skel = String::new();
 
     write!(
@@ -92,11 +281,114 @@ fn gen_skel_contents(_debug: bool, _obj: &UnprocessedObj) -> Result<String> {
         "#
     )?;
 
+    let mut obj_file_path = obj.out.clone();
+    obj_file_path.push(format!("{}.bpf.o", obj.name));
+
+    write!(
+        skel,
+        r#"
+        const DATA: &[u8] = include_bytes!("{}");
+        "#,
+        obj_file_path.as_path().display()
+    )?;
+
+    // Capitalize object name
+    let mut obj_name = String::new();
+    // Unwrap is safe b/c already checked that `obj.name` contains chars
+    obj_name += &obj.name.chars().next().unwrap().to_uppercase().to_string();
+    if obj.name.len() > 1 {
+        obj_name += &obj.name[1..];
+    }
+
+    // Open bpf_object so we can iterate over maps and progs
+    let object = open_object_file(obj_file_path.as_path())?;
+
+    write!(
+        skel,
+        r#"
+        #[derive(Default)]
+        pub struct {name}SkelBuilder {{
+            pub obj_builder: libbpf_rs::ObjectBuilder,
+            name: String,
+        }}
+
+        impl {name}SkelBuilder {{
+            pub fn name<T: AsRef<str>>(&mut self, name: T) -> &mut Self {{
+                self.name = name.as_ref().to_string();
+                self.obj_builder.name(name);
+                self
+            }}
+
+            pub fn relaxed_maps(&mut self, relaxed_maps: bool) -> &mut Self {{
+                self.obj_builder.relaxed_maps(relaxed_maps);
+                self
+            }}
+
+            pub fn debug(&mut self, dbg: bool) -> &mut Self {{
+                self.obj_builder.debug(dbg);
+                self
+            }}
+
+            pub fn open(&mut self) -> libbpf_rs::Result<Open{name}Skel> {{
+                Ok(Open{name}Skel {{
+                    obj: self.obj_builder.open_memory(&self.name, DATA)?,
+                }})
+            }}
+        }}
+        "#,
+        name = obj_name
+    )?;
+
+    gen_skel_map_defs(&mut skel, object, &obj_name, true)?;
+    gen_skel_prog_defs(&mut skel, object, &obj_name, true)?;
+
+    write!(
+        skel,
+        r#"
+        pub struct Open{name}Skel {{
+            pub obj: libbpf_rs::OpenObject,
+        }}
+
+        impl Open{name}Skel {{
+            pub fn load(&mut self) -> libbpf_rs::Result<{name}Skel> {{
+                Ok({name}Skel {{
+                    obj: self.obj.load()?,
+                }})
+            }}
+        "#,
+        name = &obj_name
+    )?;
+    gen_skel_prog_getter(&mut skel, object, &obj_name, true)?;
+    gen_skel_map_getter(&mut skel, object, &obj_name, true)?;
+    writeln!(skel, "}}")?;
+
+    gen_skel_map_defs(&mut skel, object, &obj_name, false)?;
+    gen_skel_prog_defs(&mut skel, object, &obj_name, false)?;
+
+    write!(
+        skel,
+        r#"
+        pub struct {name}Skel {{
+            pub obj: libbpf_rs::Object,
+        }}
+
+        impl {name}Skel {{
+        "#,
+        name = &obj_name,
+    )?;
+    gen_skel_prog_getter(&mut skel, object, &obj_name, false)?;
+    gen_skel_map_getter(&mut skel, object, &obj_name, false)?;
+    writeln!(skel, "}}")?;
+
     Ok(skel)
 }
 
 /// Write a single skeleton to disk
 fn gen_skel(debug: bool, obj: &UnprocessedObj) -> Result<()> {
+    if obj.name.is_empty() {
+        bail!("Object file has no name");
+    }
+
     let skel = rustfmt(&gen_skel_contents(debug, obj)?)?;
 
     let mut path = obj.path.clone();
