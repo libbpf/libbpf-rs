e0863e8e4c5b90061e9bcece27d139f66219dc5a
Daniel Xu <dxu@dxuuu.xyz>
2020-12-01 16:07:24 -0600

    libbpf-cargo: gen: Add map/prog name helpers
    Reduce code duplication later

--- libbpf-cargo/src/gen.rs
+++ libbpf-cargo/src/gen.rs
@@ -60,6 +60,26 @@ fn rustfmt(s: &str) -> Result<String> {
     Ok(String::from_utf8(output.stdout)?)
 }
 
+fn get_map_name(map: *const libbpf_sys::bpf_map) -> Result<String> {
+    let name_ptr = unsafe { libbpf_sys::bpf_map__name(map) };
+
+    if name_ptr.is_null() {
+        bail!("Map name unknown");
+    }
+
+    Ok(unsafe { CStr::from_ptr(name_ptr) }.to_str()?.to_string())
+}
+
+fn get_prog_name(prog: *const libbpf_sys::bpf_program) -> Result<String> {
+    let name_ptr = unsafe { libbpf_sys::bpf_program__name(prog) };
+
+    if name_ptr.is_null() {
+        bail!("Prog name unknown");
+    }
+
+    Ok(unsafe { CStr::from_ptr(name_ptr) }.to_str()?.to_string())
+}
+
 /// Generate contents of a single skeleton
 fn gen_skel_contents(_debug: bool, _obj: &UnprocessedObj) -> Result<String> {
     let mut skel = String::new();
