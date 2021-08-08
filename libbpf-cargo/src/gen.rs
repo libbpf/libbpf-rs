3b072c886e68171db510b715d9a8a7d592e6d4d3
Michael Mullin <mimullin@blackberry.com>
2021-08-08 16:34:30 -0400

    prevent warnings for upper case acroynms and non_camel_case_types

--- libbpf-cargo/src/gen.rs
+++ libbpf-cargo/src/gen.rs
@@ -665,7 +665,9 @@ fn gen_skel_contents(_debug: bool, raw_obj_name: &str, obj_file_path: &Path) -> 
 
            #![allow(dead_code)]
            #![allow(non_snake_case)]
+           #![allow(non_camel_case_types)]
            #![allow(clippy::transmute_ptr_to_ref)]
+           #![allow(clippy::upper_case_acronyms)]
 
            use libbpf_rs::libbpf_sys;
         "#
