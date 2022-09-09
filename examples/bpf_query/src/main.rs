50a4c49a3d175b6d561bb685850bd7eeb0ca176c
Alex Gartrell <alexgartrell@gmail.com>
2022-09-08 21:08:14 -0700

    libbpf-rs: optionally return full vecs from query
    NOTE: this is totally API-breaking, but the initial API wasn't really
    useful.
    
    This avoids allocation overhead and guessing by reading the values and
    re-invoking the system call. Each array is blocked by a feature flag
    from the iterator.
    
    The bpf_query example now adds the ability to dump disassembled code
    on x86-64 via iced-x86.

--- examples/bpf_query/src/main.rs
+++ examples/bpf_query/src/main.rs
@@ -4,11 +4,17 @@ use clap::Parser;
 use libbpf_rs::query;
 use nix::unistd::Uid;
 
+#[derive(Debug, Parser)]
+struct ProgArgs {
+    #[arg(short, long)]
+    disassemble: bool,
+}
+
 /// Query the system about BPF-related information
 #[derive(Debug, Parser)]
 enum Command {
     /// Display information about progs
-    Prog,
+    Prog(ProgArgs),
     /// Display information about maps
     Map,
     /// Display information about BTF
@@ -17,12 +23,34 @@ enum Command {
     Link,
 }
 
-fn prog() {
-    for prog in query::ProgInfoIter::default() {
+fn prog(args: ProgArgs) {
+    let opts = query::ProgInfoQueryOptions::default().include_all();
+    for prog in query::ProgInfoIter::with_query_opts(opts) {
         println!(
             "name={:<16} type={:<15} run_count={:<2} runtime_ns={}",
             prog.name, prog.ty, prog.run_cnt, prog.run_time_ns
         );
+        if args.disassemble {
+            #[cfg(target_arch = "x86_64")]
+            {
+                use iced_x86::Formatter;
+
+                let mut d = iced_x86::Decoder::new(32, &prog.jited_prog_insns, 0);
+                let mut f = iced_x86::GasFormatter::new();
+                while d.can_decode() {
+                    let ip = d.ip();
+                    let insn = d.decode();
+                    let mut f_insn = String::new();
+                    f.format(&insn, &mut f_insn);
+                    println!("  {}: {}", ip, f_insn);
+                }
+            }
+
+            #[cfg(not(target_arch = "x86_64"))]
+            {
+                println!("   Unable to disassemble on non-x86_64");
+            }
+        }
     }
 }
 
@@ -65,7 +93,7 @@ fn main() {
     let opts = Command::parse();
 
     match opts {
-        Command::Prog => prog(),
+        Command::Prog(args) => prog(args),
         Command::Map => map(),
         Command::Btf => btf(),
         Command::Link => link(),
