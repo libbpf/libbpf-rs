fd0eabde1f0048a082df4c9cc685cd0af37a6632
Daniel Müller <deso@posteo.net>
2023-07-05 13:11:30 -0700

    Make padding fields of generated types `pub`
    It arguably makes very little sense for all "regular" fields in
    libbpf-cargo generated types to be publicly accessible, while padding
    fields are not.
    Mark them as public as well. That's similar to what cbindgen does, with
    its named padding members.
    
    Closes: #421
    
    Signed-off-by: Daniel Müller <deso@posteo.net>

--- libbpf-cargo/src/test.rs
+++ libbpf-cargo/src/test.rs
@@ -1206,7 +1206,7 @@ struct Foo foo = {1};
 #[repr(C)]
 pub struct Foo {
     pub x: i32,
-    __pad_4: [u8; 12],
+    pub __pad_4: [u8; 12],
 }
 "#;
 
@@ -1307,7 +1307,7 @@ pub struct Foo {
     pub ip: *mut i32,
     pub ipp: *mut *mut i32,
     pub bar: Bar,
-    __pad_18: [u8; 6],
+    pub __pad_18: [u8; 6],
     pub pb: *mut Bar,
     pub v: u64,
     pub cv: i64,
@@ -1362,7 +1362,7 @@ pub struct Foo {
     pub ip: *mut i32,
     pub ipp: *mut *mut i32,
     pub bar: Bar,
-    __pad_84: [u8; 4],
+    pub __pad_84: [u8; 4],
     pub pb: *mut Bar,
     pub v: u64,
     pub cv: i64,
@@ -1880,10 +1880,10 @@ struct Foo foo;
 pub struct Foo {
     pub x: i32,
     pub bar: __anon_1,
-    __pad_36: [u8; 4],
+    pub __pad_36: [u8; 4],
     pub baz: __anon_2,
     pub w: i32,
-    __pad_52: [u8; 4],
+    pub __pad_52: [u8; 4],
 }
 #[derive(Copy, Clone)]
 #[repr(C)]
@@ -1962,7 +1962,7 @@ pub struct Foo {
     pub bar: __anon_1,
     pub baz: __anon_2,
     pub w: i32,
-    __pad_68: [u8; 4],
+    pub __pad_68: [u8; 4],
 }
 #[derive(Debug, Default, Copy, Clone)]
 #[repr(C)]
@@ -1974,7 +1974,7 @@ pub struct __anon_1 {
 #[repr(C)]
 pub struct __anon_2 {
     pub w: u32,
-    __pad_4: [u8; 4],
+    pub __pad_4: [u8; 4],
     pub u: *mut u64,
 }
 "#;
@@ -2027,7 +2027,7 @@ pub struct Foo {
     pub zerg: __anon_2,
     pub baz: __anon_3,
     pub w: i32,
-    __pad_76: [u8; 4],
+    pub __pad_76: [u8; 4],
     pub flarg: __anon_4,
 }
 #[derive(Debug, Default, Copy, Clone)]
@@ -2058,7 +2058,7 @@ impl Default for __anon_2 {
 #[repr(C)]
 pub struct __anon_3 {
     pub w: u32,
-    __pad_4: [u8; 4],
+    pub __pad_4: [u8; 4],
     pub u: *mut u64,
 }
 #[derive(Copy, Clone)]
@@ -2202,7 +2202,7 @@ struct bpf_sock_tuple_5_15 tup;
 #[repr(C)]
 pub struct bpf_sock_tuple_5_15 {
     pub __anon_1: __anon_1,
-    __pad_36: [u8; 4],
+    pub __pad_36: [u8; 4],
     pub __anon_2: __anon_2,
 }
 #[derive(Copy, Clone)]
