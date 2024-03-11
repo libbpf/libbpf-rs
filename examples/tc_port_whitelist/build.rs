a0125dc618757cb8c35f2afc1728797565f4cdfc
Daniel Müller <deso@posteo.net>
2024-03-11 11:01:05 -0700

    Use `CARGO_CFG_TARGET_ARCH` environment variable in example build scripts
    It appears as if `std::env::consts::ARCH` does not get updated to the
    target architecture in a cross-compilation context. Switch to using the
    `CARGO_CFG_TARGET_ARCH` environment variable in the hopes that we get
    the desired behavior.
    
    Signed-off-by: Daniel Müller <deso@posteo.net>

--- examples/tc_port_whitelist/build.rs
+++ examples/tc_port_whitelist/build.rs
@@ -1,5 +1,4 @@
 use std::env;
-use std::env::consts::ARCH;
 use std::ffi::OsStr;
 use std::path::Path;
 use std::path::PathBuf;
@@ -12,11 +11,15 @@ fn main() {
     let mut out =
         PathBuf::from(env::var_os("OUT_DIR").expect("OUT_DIR must be set in build script"));
     out.push("tc.skel.rs");
+
+    let arch = env::var("CARGO_CFG_TARGET_ARCH")
+        .expect("CARGO_CFG_TARGET_ARCH must be set in build script");
+
     SkeletonBuilder::new()
         .source(SRC)
         .clang_args([
             OsStr::new("-I"),
-            Path::new("../vmlinux").join(ARCH).as_os_str(),
+            Path::new("../vmlinux").join(arch).as_os_str(),
         ])
         .build_and_generate(&out)
         .unwrap();
