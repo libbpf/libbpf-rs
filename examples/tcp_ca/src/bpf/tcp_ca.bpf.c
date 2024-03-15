8999e471ddb72eef45c14dea8d4662559d6f8d38
Daniel Müller <deso@posteo.net>
2024-03-15 15:37:44 -0700

    Adjust tcp_ca example to use struct_ops shadow data
    This change adjusts the tcp_ca example to include the usage of
    struct_ops shadow data. Specifically, we modify the name under which we
    register the algorithm before attaching the program. We then make sure
    that we can use the algorithm under the new name. In addition, we also
    illustrate setting of a callback/program before load.
    
    Signed-off-by: Daniel Müller <deso@posteo.net>

--- examples/tcp_ca/src/bpf/tcp_ca.bpf.c
+++ examples/tcp_ca/src/bpf/tcp_ca.bpf.c
@@ -8,6 +8,7 @@
 char _license[] SEC("license") = "GPL";
 
 int ca_cnt = 0;
+bool cong_control = false;
 
 static inline struct tcp_sock *tcp_sk(const struct sock *sk)
 {
@@ -20,10 +21,22 @@ void BPF_PROG(ca_update_init, struct sock *sk)
 	ca_cnt++;
 }
 
+SEC("struct_ops/ca_update_2_init")
+void BPF_PROG(ca_update_2_init, struct sock *sk)
+{
+}
+
 SEC("struct_ops/ca_update_cong_control")
 void BPF_PROG(ca_update_cong_control, struct sock *sk,
 	      const struct rate_sample *rs)
 {
+}
+
+SEC("struct_ops/ca_update_cong_control2")
+void BPF_PROG(ca_update_cong_control2, struct sock *sk,
+	      const struct rate_sample *rs)
+{
+  cong_control = true;
 }
 
 SEC("struct_ops/ca_update_ssthresh")
@@ -38,11 +51,25 @@ __u32 BPF_PROG(ca_update_undo_cwnd, struct sock *sk)
 	return tcp_sk(sk)->snd_cwnd;
 }
 
-SEC(".struct_ops")
+SEC(".struct_ops.link")
 struct tcp_congestion_ops ca_update = {
 	.init = (void *)ca_update_init,
 	.cong_control = (void *)ca_update_cong_control,
 	.ssthresh = (void *)ca_update_ssthresh,
 	.undo_cwnd = (void *)ca_update_undo_cwnd,
 	.name = "tcp_ca_update",
+};
+
+SEC(".struct_ops")
+struct tcp_congestion_ops ca_update_2 = {
+	.init = (void *)ca_update_2_init,
+	.cong_control = (void *)ca_update_cong_control2,
+	.ssthresh = (void *)ca_update_ssthresh,
+	.undo_cwnd = (void *)ca_update_undo_cwnd,
+	.name = "tcp_ca_update_2",
+};
+
+SEC(".struct_ops.link")
+struct tcp_congestion_ops ca_wrong = {
+	.cong_control = (void *)ca_update_cong_control,
 };
