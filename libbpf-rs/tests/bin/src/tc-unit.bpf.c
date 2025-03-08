b12aa9e0944d17c73f60a626be14b89fc3d3b308
Hengqi Chen <hengqi.chen@gmail.com>
2025-03-08 05:36:07 +0000

    libbpf-rs: Format BPF test codes
    Format the test code with:
        clang-format -i libbpf-rs/tests/bin/src/*
    
    Signed-off-by: Hengqi Chen <hengqi.chen@gmail.com>

--- libbpf-rs/tests/bin/src/tc-unit.bpf.c
+++ libbpf-rs/tests/bin/src/tc-unit.bpf.c
@@ -1,12 +1,11 @@
 #include "vmlinux.h"
 #include <bpf/bpf_helpers.h>
 
-//#include <linux/pkt_cls.h>
-#define TC_ACT_UNSPEC	(-1)
+// #include <linux/pkt_cls.h>
+#define TC_ACT_UNSPEC (-1)
 
 SEC("tc")
 int handle_tc(struct __sk_buff *skb)
 {
-    return TC_ACT_UNSPEC;
+	return TC_ACT_UNSPEC;
 }
-
